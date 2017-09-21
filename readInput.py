aa51142d1155bd3bea7e6540bf59f179c1159c30
Arjan <arjanboogaart@gmail.com>
2017-09-21 11:22:11 +0200

    Find closest points

--- readInput.py
+++ readInput.py
@@ -1,7 +1,9 @@
 import rhinoscriptsyntax as rs
+from itertools import izip, islice
 
 buildings = [] #Set of buildings
 plot = [] #Plot points
+closestEdges = [] #Closest edge for each building to the plot
 
 ##
 # Translate 2 diagonal points into the 8 box coordinates
@@ -17,7 +19,7 @@ def readFile():
             p1 = f.readline().rstrip().split(",")
             p2 = f.readline().rstrip().split(",")
             rot = f.readline().rstrip()
-            drawBox(p1, p2, rot)
+            createBox(p1,p2,rot)
         if(line == "Plot\n"): #Plot has 2 3D points
             p1 = f.readline().rstrip().split(",")
             p2 = f.readline().rstrip().split(",")
@@ -27,16 +29,23 @@ def readFile():
     f.close()
 
 ##
-# Draw a box given 2 diagonal points and rotation in degrees
-def drawBox(p1, p2, rot):
+# Translate 2 points and rotation to 8 box coordinates
+def createBox(p1,p2,rot):
     box = cornersToBox(p1,p2)
     center = rs.PointDivide(rs.PointAdd(p1,p2), 2)
     xform = rs.XformRotation2(float(rot), (0,0,1), center)
     newBox = []
     for point in box:
         newBox.append(rs.PointTransform(point, xform))
-    rs.AddBox(newBox)
     buildings.append(newBox)
+    return newBox
+
+##
+# Draws set of buildings
+def drawBuildings(buildings):
+    for box in buildings:
+        rs.AddBox(box)
+
 
 ##
 # Draw a rectangle given 2 points
@@ -46,9 +55,28 @@ def drawPlot(p1, p2):
     rs.AddLine(p2, (p1[0], p2[1], p1[2]))
     rs.AddLine(p2, (p2[0], p1[1], p1[2]))
 
-#def findClosestEdges(p1, p2):
-#    for building in buildings:
-#        for point in building:
-#            
+##
+# Finds the closest edge for all buildings and adds it to closestEdges
+def findClosestEdges(p1, p2):
+    for building in buildings:
+        for edge in getEdges(building):
+            min = float("Inf")
+            dist = rs.LineMinDistanceTo(edge, rs.PointDivide(rs.PointAdd(p1, p2),2))
+            closest = None
+            if(dist < min):
+                min = dist
+                closest = edge
+            closestEdges.append(edge)
+
+##
+# Returns 4 lines that define the base of the building
+def getEdges(building):
+    edges = []
+    for bp1,bp2 in izip(building, islice(building,1,4)): #only take edges from base
+        edges.append([bp1, bp2])
+    edges.append([building[3], building[0]])
+    return edges
 
 readFile()
+drawBuildings(buildings)
+findClosestEdges(plot[0],plot[1])
