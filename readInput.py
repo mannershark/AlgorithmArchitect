c0bd66e55299b377c7dbd4b1260ff21fc8142f4b
Arjan <arjanboogaart@gmail.com>
2017-09-21 09:49:15 +0200

    Initial Example

--- readInput.py
+++ readInput.py
@@ -1,6 +1,7 @@
 import rhinoscriptsyntax as rs
 
 buildings = []
+plot = []
 
 def cornersToBox(x, y):
     return [(x[0],x[1],x[2]),(y[0],x[1],x[2]),(y[0],y[1],x[2]),(x[0],y[1],x[2]),(x[0],x[1],y[2]),(y[0],x[1],y[2]),(y[0],y[1],y[2]),(x[0],y[1],y[2])]
@@ -13,6 +14,12 @@ def readFile():
             y = f.readline().rstrip().split(",")
             rot = f.readline().rstrip()
             drawBox(x, y, rot)
+        if(line == "Plot\n"):
+            x = f.readline().rstrip().split(",")
+            y = f.readline().rstrip().split(",")
+            plot.append(x)
+            plot.append(y)
+            drawBox(x, y, 0)
     f.close()
 
 def drawBox(x, y, rot):
