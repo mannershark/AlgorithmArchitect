cc45cba51c3802eee13cdc93ec9e47bf9e545923
Arjan <arjanboogaart@gmail.com>
2017-09-21 10:51:37 +0200

    Added comments

--- readInput.py
+++ readInput.py
@@ -1,20 +1,24 @@
 import rhinoscriptsyntax as rs
 
-buildings = []
-plot = []
+buildings = [] #Set of buildings
+plot = [] #Plot points
 
+##
+# Translate 2 diagonal points into the 8 box coordinates
 def cornersToBox(p1, p2):
     return [(p1[0],p1[1],p1[2]),(p2[0],p1[1],p1[2]),(p2[0],p2[1],p1[2]),(p1[0],p2[1],p1[2]),(p1[0],p1[1],p2[2]),(p2[0],p1[1],p2[2]),(p2[0],p2[1],p2[2]),(p1[0],p2[1],p2[2])]
 
+##
+# Read from file and draw it's contents
 def readFile():
     f = open('test1.txt', 'r')
     for line in f:
-        if(line == 'Building\n'):
+        if(line == 'Building\n'): #Buildings have 2 3D points and a rotation in degrees
             p1 = f.readline().rstrip().split(",")
             p2 = f.readline().rstrip().split(",")
             rot = f.readline().rstrip()
             drawBox(p1, p2, rot)
-        if(line == "Plot\n"):
+        if(line == "Plot\n"): #Plot has 2 3D points
             p1 = f.readline().rstrip().split(",")
             p2 = f.readline().rstrip().split(",")
             plot.append(p1)
@@ -22,6 +26,8 @@ def readFile():
             drawPlot(p1, p2)
     f.close()
 
+##
+# Draw a box given 2 diagonal points and rotation in degrees
 def drawBox(p1, p2, rot):
     box = cornersToBox(p1,p2)
     center = rs.PointDivide(rs.PointAdd(p1,p2), 2)
@@ -32,6 +38,8 @@ def drawBox(p1, p2, rot):
     rs.AddBox(newBox)
     buildings.append(newBox)
 
+##
+# Draw a rectangle given 2 points
 def drawPlot(p1, p2):
     rs.AddLine(p1, (p1[0], p2[1], p1[2]))
     rs.AddLine(p1, (p2[0], p1[1], p1[2]))
