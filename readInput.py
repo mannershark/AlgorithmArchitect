b5299b38d14c9339d3a318870ae2e60fa47a9774
Arjan <arjanboogaart@gmail.com>
2017-09-21 10:45:49 +0200

    Change x,y to p1, p2

--- readInput.py
+++ readInput.py
@@ -3,28 +3,28 @@ import rhinoscriptsyntax as rs
 buildings = []
 plot = []
 
-def cornersToBox(x, y):
-    return [(x[0],x[1],x[2]),(y[0],x[1],x[2]),(y[0],y[1],x[2]),(x[0],y[1],x[2]),(x[0],x[1],y[2]),(y[0],x[1],y[2]),(y[0],y[1],y[2]),(x[0],y[1],y[2])]
+def cornersToBox(p1, p2):
+    return [(p1[0],p1[1],p1[2]),(p2[0],p1[1],p1[2]),(p2[0],p2[1],p1[2]),(p1[0],p2[1],p1[2]),(p1[0],p1[1],p2[2]),(p2[0],p1[1],p2[2]),(p2[0],p2[1],p2[2]),(p1[0],p2[1],p2[2])]
 
 def readFile():
-    f = open('test1.txt', 'r')
+    f = open('test1.tp1t', 'r')
     for line in f:
         if(line == 'Building\n'):
-            x = f.readline().rstrip().split(",")
-            y = f.readline().rstrip().split(",")
+            p1 = f.readline().rstrip().split(",")
+            p2 = f.readline().rstrip().split(",")
             rot = f.readline().rstrip()
-            drawBox(x, y, rot)
+            drawBop1(p1, p2, rot)
         if(line == "Plot\n"):
-            x = f.readline().rstrip().split(",")
-            y = f.readline().rstrip().split(",")
-            plot.append(x)
-            plot.append(y)
-            drawPlot(x, y)
+            p1 = f.readline().rstrip().split(",")
+            p2 = f.readline().rstrip().split(",")
+            plot.append(p1)
+            plot.append(p2)
+            drawPlot(p1, p2)
     f.close()
 
-def drawBox(x, y, rot):
-    box = cornersToBox(x,y)
-    center = rs.PointDivide(rs.PointAdd(x,y), 2)
+def drawBox(p1, p2, rot):
+    box = cornersToBox(p1,p2)
+    center = rs.PointDivide(rs.PointAdd(p1,p2), 2)
     xform = rs.XformRotation2(float(rot), (0,0,1), center)
     newBox = []
     for point in box:
@@ -32,13 +32,13 @@ def drawBox(x, y, rot):
     rs.AddBox(newBox)
     buildings.append(newBox)
 
-def drawPlot(x, y):
-    rs.AddLine(x, (x[0], y[1], x[2]))
-    rs.AddLine(x, (y[0], x[1], x[2]))
-    rs.AddLine(y, (x[0], y[1], x[2]))
-    rs.AddLine(y, (y[0], x[1], x[2]))
+def drawPlot(p1, p2):
+    rs.AddLine(p1, (p1[0], p2[1], p1[2]))
+    rs.AddLine(p1, (p2[0], p1[1], p1[2]))
+    rs.AddLine(p2, (p1[0], p2[1], p1[2]))
+    rs.AddLine(p2, (p2[0], p1[1], p1[2]))
 
-#def findClosestEdges(x, y):
+#def findClosestEdges(p1, p2):
 #    for building in buildings:
 #        for point in building:
 #            
